b014e191d0662472ccc932b8ac3f33319b509d0d
crazy-coding <dennischo122@gmail.com>
2020-02-26 05:33:27 +0800

    bug fix

--- crypto/xrp.py
+++ crypto/xrp.py
@@ -6,6 +6,7 @@ from .models import XRP
 from eth_account import Account as eaAccount
 # from django.core import serializers
 from ripple_api import RippleRPCClient
+from django.utils.crypto import get_random_string
 
 # RIPPLE_SERVER = 'http://r.ripple.com:51235/' # local
 RIPPLE_SERVER = 'http://s.altnet.ripple.com:51235/' # testnet local
